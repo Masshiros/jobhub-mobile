d9f51412776de0f0f91c0da3d2b6ce9f7500283e
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-08-02 13:01:28 +0700

    Feat(auth) : Finish login & signup form

--- lib/controllers/signup_provider.dart
+++ lib/controllers/signup_provider.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
-
-
 class SignUpNotifier extends ChangeNotifier {
 // trigger to hide and unhide the password
-  bool _isObsecure = true;
+  bool _obscureText = true;
 
-  bool get isObsecure => _isObsecure;
+  bool get obscureText => _obscureText;
 
-  set isObsecure(bool obsecure) {
-    _isObsecure = obsecure;
+  set obscureText(bool newState) {
+    _obscureText = newState;
     notifyListeners();
   }
 
@@ -36,12 +34,12 @@ class SignUpNotifier extends ChangeNotifier {
   final signupFormKey = GlobalKey<FormState>();
 
   bool passwordValidator(String password) {
-  if (password.isEmpty) return false;
-  String pattern =
-      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
-  RegExp regex = RegExp(pattern);
-  return regex.hasMatch(password);
-}
+    if (password.isEmpty) return false;
+    String pattern =
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+    RegExp regex = RegExp(pattern);
+    return regex.hasMatch(password);
+  }
 
   bool validateAndSave() {
     final form = signupFormKey.currentState;
@@ -52,5 +50,4 @@ class SignUpNotifier extends ChangeNotifier {
       return false;
     }
   }
-
 }
