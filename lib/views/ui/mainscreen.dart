fc9123e6f37f6e71af03105f7043b0e3b260c1a8
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-08-03 11:24:12 +0700

    Feat(home): Custom drawer side menu bar

--- lib/views/ui/mainscreen.dart
+++ lib/views/ui/mainscreen.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:jobhub/constants/app_constants.dart';
+import 'package:jobhub/controllers/exports.dart';
+import 'package:jobhub/views/common/drawer/drawer-screen.dart';
+import 'package:jobhub/views/ui/auth/profile.dart';
+import 'package:jobhub/views/ui/bookmarks/bookmarks.dart';
+import 'package:jobhub/views/ui/chat/chat_list.dart';
+import 'package:jobhub/views/ui/device_mgt/devices_info.dart';
+import 'package:jobhub/views/ui/homepage.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,10 +18,38 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  
-  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Consumer<ZoomNotifier>(builder: (context, controller, child) {
+      return ZoomDrawer(
+        menuScreen: DrawerScreen(indexSetter: (index) {
+          controller.currentIndex = index;
+        }),
+        mainScreen: currentScreen(),
+        borderRadius: 30,
+        showShadow: true,
+        angle: 0.0,
+        slideWidth: 250,
+        menuBackgroundColor: Color(kLightBlue.value),
+      );
+    });
+  }
+
+  Widget currentScreen() {
+    var zoomNotifier = Provider.of<ZoomNotifier>(context);
+    switch (zoomNotifier.currentIndex) {
+      case 0:
+        return const HomePage();
+      case 1:
+        return const ChatsList();
+      case 2:
+        return const BookMarkPage();
+      case 3:
+        return const DeviceManagement();
+      case 4:
+        return const ProfilePage();
+      default:
+        return const HomePage();
+    }
   }
 }
